22cf5545823713e05ab59bc70693d8fe01dd8fb3
Tsodenhist <thomassoden@live.co.uk>
2026-05-29 11:56:00 +0000

    Deploying to gh-pages from @ Tsodenhist/tsodenhist.github.io@7a3b12e7717183218f508cee1eb906f546b540ce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "My full CV can be provided on request via email.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
